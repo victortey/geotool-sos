53557bc0f176c0f6b3c58df6fec99883ed7d0062
Victor Tey <Victor.Tey@csiro.au>
2012-09-28 17:34:32 +0800

    update sos and configuration file

--- modules/unsupported/xsd-sos/src/main/java/org/geotools/sos/v2_0_0_Binding/SOS.java
+++ modules/unsupported/xsd-sos/src/main/java/org/geotools/sos/v2_0_0_Binding/SOS.java
@@ -43,7 +43,7 @@ public final class SOS extends XSD {
      * Returns the location of 'sosInsertSensor.xsd.'.
      */
     public String getSchemaLocation() {
-       return getClass().getResource("sosInsertSensor.xsd").toString();
+       return getClass().getResource("sos.xsd").toString();
     }
     
     /** @generated */
@@ -56,12 +56,268 @@ public final class SOS extends XSD {
     /** @generated */
     public static final QName SosInsertionMetadataType = 
         new QName("http://www.opengis.net/sos/2.0","SosInsertionMetadataType");
+    /** @generated */
+    public static final QName GetObservationByIdPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdPropertyType");
+    /** @generated */
+    public static final QName GetObservationByIdResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdResponsePropertyType");
+    /** @generated */
+    public static final QName GetObservationByIdResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdResponseType");
+    /** @generated */
+    public static final QName GetObservationByIdType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdType");
+    /** @generated */
+    public static final QName GetObservationByIdResponseType_observation = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdResponseType_observation");
+    /** @generated */
+    public static final QName CapabilitiesPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","CapabilitiesPropertyType");
+    /** @generated */
+    public static final QName CapabilitiesType = 
+        new QName("http://www.opengis.net/sos/2.0","CapabilitiesType");
+    /** @generated */
+    public static final QName GetCapabilitiesPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetCapabilitiesPropertyType");
+    /** @generated */
+    public static final QName GetCapabilitiesType = 
+        new QName("http://www.opengis.net/sos/2.0","GetCapabilitiesType");
+    /** @generated */
+    public static final QName CapabilitiesType_filterCapabilities = 
+        new QName("http://www.opengis.net/sos/2.0","CapabilitiesType_filterCapabilities");
+    /** @generated */
+    public static final QName CapabilitiesType_contents = 
+        new QName("http://www.opengis.net/sos/2.0","CapabilitiesType_contents");
+    /** @generated */
+    public static final QName ContentsPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","ContentsPropertyType");
+    /** @generated */
+    public static final QName ContentsType = 
+        new QName("http://www.opengis.net/sos/2.0","ContentsType");
+    /** @generated */
+    public static final QName ObservationOfferingPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOfferingPropertyType");
+    /** @generated */
+    public static final QName ObservationOfferingType = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOfferingType");
+    /** @generated */
+    public static final QName ObservationOfferingType_observedArea = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOfferingType_observedArea");
+    /** @generated */
+    public static final QName ObservationOfferingType_phenomenonTime = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOfferingType_phenomenonTime");
+    /** @generated */
+    public static final QName ObservationOfferingType_resultTime = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOfferingType_resultTime");
+    /** @generated */
+    public static final QName GetFeatureOfInterestPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestPropertyType");
+    /** @generated */
+    public static final QName GetFeatureOfInterestResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestResponsePropertyType");
+    /** @generated */
+    public static final QName GetFeatureOfInterestResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestResponseType");
+    /** @generated */
+    public static final QName GetFeatureOfInterestType = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestType");
+    /** @generated */
+    public static final QName GetFeatureOfInterestType_spatialFilter = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestType_spatialFilter");
+    /** @generated */
+    public static final QName GetObservationPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationPropertyType");
+    /** @generated */
+    public static final QName GetObservationResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationResponsePropertyType");
+    /** @generated */
+    public static final QName GetObservationResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationResponseType");
+    /** @generated */
+    public static final QName GetObservationType = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationType");
+    /** @generated */
+    public static final QName GetObservationResponseType_observationData = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationResponseType_observationData");
+    /** @generated */
+    public static final QName GetObservationType_temporalFilter = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationType_temporalFilter");
+    /** @generated */
+    public static final QName GetObservationType_spatialFilter = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationType_spatialFilter");
+    /** @generated */
+    public static final QName GetResultPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultPropertyType");
+    /** @generated */
+    public static final QName GetResultResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultResponsePropertyType");
+    /** @generated */
+    public static final QName GetResultResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultResponseType");
+    /** @generated */
+    public static final QName GetResultType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultType");
+    /** @generated */
+    public static final QName GetResultType_temporalFilter = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultType_temporalFilter");
+    /** @generated */
+    public static final QName GetResultType_spatialFilter = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultType_spatialFilter");
+    /** @generated */
+    public static final QName GetResultTemplatePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplatePropertyType");
+    /** @generated */
+    public static final QName GetResultTemplateResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateResponsePropertyType");
+    /** @generated */
+    public static final QName GetResultTemplateResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateResponseType");
+    /** @generated */
+    public static final QName GetResultTemplateType = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateType");
+    /** @generated */
+    public static final QName GetResultTemplateResponseType_resultStructure = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateResponseType_resultStructure");
+    /** @generated */
+    public static final QName GetResultTemplateResponseType_resultEncoding = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateResponseType_resultEncoding");
+    /** @generated */
+    public static final QName InsertionCapabilitiesPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertionCapabilitiesPropertyType");
+    /** @generated */
+    public static final QName InsertionCapabilitiesType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertionCapabilitiesType");
+    /** @generated */
+    public static final QName InsertObservationPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationPropertyType");
+    /** @generated */
+    public static final QName InsertObservationResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationResponsePropertyType");
+    /** @generated */
+    public static final QName InsertObservationResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationResponseType");
+    /** @generated */
+    public static final QName InsertObservationType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationType");
+    /** @generated */
+    public static final QName InsertObservationType_observation = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationType_observation");
+    /** @generated */
+    public static final QName InsertResultPropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultPropertyType");
+    /** @generated */
+    public static final QName InsertResultResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultResponsePropertyType");
+    /** @generated */
+    public static final QName InsertResultResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultResponseType");
+    /** @generated */
+    public static final QName InsertResultType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultType");
+    /** @generated */
+    public static final QName InsertResultTemplatePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplatePropertyType");
+    /** @generated */
+    public static final QName InsertResultTemplateResponsePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplateResponsePropertyType");
+    /** @generated */
+    public static final QName InsertResultTemplateResponseType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplateResponseType");
+    /** @generated */
+    public static final QName InsertResultTemplateType = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplateType");
+    /** @generated */
+    public static final QName ResultTemplatePropertyType = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplatePropertyType");
+    /** @generated */
+    public static final QName ResultTemplateType = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplateType");
+    /** @generated */
+    public static final QName InsertResultTemplateType_proposedTemplate = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplateType_proposedTemplate");
+    /** @generated */
+    public static final QName ResultTemplateType_observationTemplate = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplateType_observationTemplate");
+    /** @generated */
+    public static final QName ResultTemplateType_resultStructure = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplateType_resultStructure");
+    /** @generated */
+    public static final QName ResultTemplateType_resultEncoding = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplateType_resultEncoding");
 
     /* Elements */
     /** @generated */
     public static final QName SosInsertionMetadata = 
         new QName("http://www.opengis.net/sos/2.0","SosInsertionMetadata");
+    /** @generated */
+    public static final QName GetObservationById = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationById");
+    /** @generated */
+    public static final QName GetObservationByIdResponse = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationByIdResponse");
+    /** @generated */
+    public static final QName Capabilities = 
+        new QName("http://www.opengis.net/sos/2.0","Capabilities");
+    /** @generated */
+    public static final QName GetCapabilities = 
+        new QName("http://www.opengis.net/sos/2.0","GetCapabilities");
+    /** @generated */
+    public static final QName Contents = 
+        new QName("http://www.opengis.net/sos/2.0","Contents");
+    /** @generated */
+    public static final QName ObservationOffering = 
+        new QName("http://www.opengis.net/sos/2.0","ObservationOffering");
+    /** @generated */
+    public static final QName GetFeatureOfInterest = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterest");
+    /** @generated */
+    public static final QName GetFeatureOfInterestResponse = 
+        new QName("http://www.opengis.net/sos/2.0","GetFeatureOfInterestResponse");
+    /** @generated */
+    public static final QName GetObservation = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservation");
+    /** @generated */
+    public static final QName GetObservationResponse = 
+        new QName("http://www.opengis.net/sos/2.0","GetObservationResponse");
+    /** @generated */
+    public static final QName GetResult = 
+        new QName("http://www.opengis.net/sos/2.0","GetResult");
+    /** @generated */
+    public static final QName GetResultResponse = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultResponse");
+    /** @generated */
+    public static final QName GetResultTemplate = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplate");
+    /** @generated */
+    public static final QName GetResultTemplateResponse = 
+        new QName("http://www.opengis.net/sos/2.0","GetResultTemplateResponse");
+    /** @generated */
+    public static final QName InsertionCapabilities = 
+        new QName("http://www.opengis.net/sos/2.0","InsertionCapabilities");
+    /** @generated */
+    public static final QName InsertObservation = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservation");
+    /** @generated */
+    public static final QName InsertObservationResponse = 
+        new QName("http://www.opengis.net/sos/2.0","InsertObservationResponse");
+    /** @generated */
+    public static final QName InsertResult = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResult");
+    /** @generated */
+    public static final QName InsertResultResponse = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultResponse");
+    /** @generated */
+    public static final QName InsertResultTemplate = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplate");
+    /** @generated */
+    public static final QName InsertResultTemplateResponse = 
+        new QName("http://www.opengis.net/sos/2.0","InsertResultTemplateResponse");
+    /** @generated */
+    public static final QName ResultTemplate = 
+        new QName("http://www.opengis.net/sos/2.0","ResultTemplate");
 
+    
     /* Attributes */
 
 }
--- modules/unsupported/xsd-sos/src/main/java/org/geotools/sos/v2_0_0_Binding/SOSConfiguration.java
+++ modules/unsupported/xsd-sos/src/main/java/org/geotools/sos/v2_0_0_Binding/SOSConfiguration.java
@@ -31,8 +31,71 @@ public class SOSConfiguration extends Configuration {
         //Types
         container.registerComponentImplementation(SOS.SosInsertionMetadataPropertyType,SosInsertionMetadataPropertyTypeBinding.class);
         container.registerComponentImplementation(SOS.SosInsertionMetadataType,SosInsertionMetadataTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationByIdPropertyType,GetObservationByIdPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationByIdResponsePropertyType,GetObservationByIdResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationByIdResponseType,GetObservationByIdResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationByIdType,GetObservationByIdTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationByIdResponseType_observation,GetObservationByIdResponseType_observationBinding.class);
+        container.registerComponentImplementation(SOS.CapabilitiesPropertyType,CapabilitiesPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.CapabilitiesType,CapabilitiesTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetCapabilitiesPropertyType,GetCapabilitiesPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetCapabilitiesType,GetCapabilitiesTypeBinding.class);
+        container.registerComponentImplementation(SOS.CapabilitiesType_filterCapabilities,CapabilitiesType_filterCapabilitiesBinding.class);
+        container.registerComponentImplementation(SOS.CapabilitiesType_contents,CapabilitiesType_contentsBinding.class);
+        container.registerComponentImplementation(SOS.ContentsPropertyType,ContentsPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.ContentsType,ContentsTypeBinding.class);
+        container.registerComponentImplementation(SOS.ObservationOfferingPropertyType,ObservationOfferingPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.ObservationOfferingType,ObservationOfferingTypeBinding.class);
+        container.registerComponentImplementation(SOS.ObservationOfferingType_observedArea,ObservationOfferingType_observedAreaBinding.class);
+        container.registerComponentImplementation(SOS.ObservationOfferingType_phenomenonTime,ObservationOfferingType_phenomenonTimeBinding.class);        
+        container.registerComponentImplementation(SOS.ObservationOfferingType_resultTime,ObservationOfferingType_resultTimeBinding.class);
+        container.registerComponentImplementation(SOS.GetFeatureOfInterestPropertyType,GetFeatureOfInterestPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetFeatureOfInterestResponsePropertyType,GetFeatureOfInterestResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetFeatureOfInterestResponseType,GetFeatureOfInterestResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetFeatureOfInterestType,GetFeatureOfInterestTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetFeatureOfInterestType_spatialFilter,GetFeatureOfInterestType_spatialFilterBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationPropertyType,GetObservationPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationResponsePropertyType,GetObservationResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationResponseType,GetObservationResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationType,GetObservationTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationResponseType_observationData,GetObservationResponseType_observationDataBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationType_temporalFilter,GetObservationType_temporalFilterBinding.class);
+        container.registerComponentImplementation(SOS.GetObservationType_spatialFilter,GetObservationType_spatialFilterBinding.class);
+        container.registerComponentImplementation(SOS.GetResultPropertyType,GetResultPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultResponsePropertyType,GetResultResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultResponseType,GetResultResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultType,GetResultTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultType_temporalFilter,GetResultType_temporalFilterBinding.class);
+        container.registerComponentImplementation(SOS.GetResultType_spatialFilter,GetResultType_spatialFilterBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplatePropertyType,GetResultTemplatePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplateResponsePropertyType,GetResultTemplateResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplateResponseType,GetResultTemplateResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplateType,GetResultTemplateTypeBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplateResponseType_resultStructure,GetResultTemplateResponseType_resultStructureBinding.class);
+        container.registerComponentImplementation(SOS.GetResultTemplateResponseType_resultEncoding,GetResultTemplateResponseType_resultEncodingBinding.class);
+        container.registerComponentImplementation(SOS.InsertionCapabilitiesPropertyType,InsertionCapabilitiesPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertionCapabilitiesType,InsertionCapabilitiesTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertObservationPropertyType,InsertObservationPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertObservationResponsePropertyType,InsertObservationResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertObservationResponseType,InsertObservationResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertObservationType,InsertObservationTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertObservationType_observation,InsertObservationType_observationBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultPropertyType,InsertResultPropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultResponsePropertyType,InsertResultResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultResponseType,InsertResultResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultType,InsertResultTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultTemplatePropertyType,InsertResultTemplatePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultTemplateResponsePropertyType,InsertResultTemplateResponsePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultTemplateResponseType,InsertResultTemplateResponseTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultTemplateType,InsertResultTemplateTypeBinding.class);
+        container.registerComponentImplementation(SOS.ResultTemplatePropertyType,ResultTemplatePropertyTypeBinding.class);
+        container.registerComponentImplementation(SOS.ResultTemplateType,ResultTemplateTypeBinding.class);
+        container.registerComponentImplementation(SOS.InsertResultTemplateType_proposedTemplate,InsertResultTemplateType_proposedTemplateBinding.class);
+        container.registerComponentImplementation(SOS.ResultTemplateType_observationTemplate,ResultTemplateType_observationTemplateBinding.class);
+        container.registerComponentImplementation(SOS.ResultTemplateType_resultStructure,ResultTemplateType_resultStructureBinding.class);
+        container.registerComponentImplementation(SOS.ResultTemplateType_resultEncoding,ResultTemplateType_resultEncodingBinding.class);
 
-
-    
+        
+        
     }
 } 
